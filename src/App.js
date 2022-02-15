7d2f3adafea3d367d53dab37a3de75c754d9f327
Anastasiia Smith <anastasiiasmithdev@gmail.com>
2022-02-14 19:30:32 -0500

    Added and mapped the data.js, implemented into the props, changed styles for cards, moved some of the images to the public folder to be able to render them on the page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import Header from './Header.js'
 import Main from './Main.js'
 import Card from './Card.js'
+import data from './data.js'
 
 export default function App() {
+  const dataCards = data.map(item => {
+      return <Card 
+              key={item.id}
+              img={item.coverImg}
+              rating={item.stats.rating}
+              reviewCount={item.stats.reviewCount}
+              location={item.location}
+              title={item.title}
+              description={item.description}
+              price={item.price}
+              openSpots={item.openSpots}
+            />
+      
+  })
   return(
     <div className="container">
       <Header />
       <Main />
       <div className="cards">
-        <Card />
+        {dataCards}
       </div>
     </div>
   )
